a7b86e0218aabbb4e4ea3e38c8d2d74a7ed024b7
Yisak Yoon <esakyy@gmail.com>
2025-11-26 21:02:00 -0500

    5

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,7 @@
 public class helloworld {
 public static void main (String [] args){
     System.out.println("hello world");
-    System.out.println("n");
-    //comment
-    //committ
+    
     
 }
 }
